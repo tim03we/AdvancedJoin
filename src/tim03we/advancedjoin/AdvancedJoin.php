759a7b54f301fcbe6287781976b188e54850f519
Tim <tim.wenderholm1931@gmail.com>
2019-05-25 15:47:00 +0200

    oops again..

--- src/tim03we/advancedjoin/AdvancedJoin.php
+++ src/tim03we/advancedjoin/AdvancedJoin.php
@@ -13,7 +13,6 @@ class AdvancedJoin extends PluginBase implements Listener {
 
     public function configUpdater() : void {
 		if($this->cfg->get("version") !== "1.1.2"){
-		if($this->cfg->get("version") !== "1.1.1"){
 			rename($this->getDataFolder() . "settings.yml", $this->getDataFolder() . "settings_old.yml");
 			$this->saveResource("settings.yml");
             $this->getLogger()->notice("We create a new settings.yml file for you.");
